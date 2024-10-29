a84deb038678239d23efd9e18e0c22026f9ae6e9
EllieThePink <133701085+elliethepink@users.noreply.github.com>
2024-10-29 20:30:24 +0000

    r109

--- src/apiConnector.ts
+++ src/apiConnector.ts
@@ -68,7 +68,7 @@ export interface SyncMapDataPayload {
 }
 
 // What the bot advertises as its game version
-const GAMEVERSION = "R108";
+const GAMEVERSION = "R109";
 const LZSTRING_MAGIC = "╬";
 
 class PromiseResolve<T> {
